f913f6afd87d48a8165491a2b9d52492b0a8a10a
Jesse Hallett <hallettj@gmail.com>
2010-09-14 00:41:25 -0700

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,53 @@
+Party Mix for Rdio
+====================
+
+UserScript for Rdio that adds a button to your Collection view that adds 25
+random songs from your collection to your play queue.
+
+I enjoy Rdio; but I thought that it was really missing a "shuffle all" feature.
+So I put this script together.
+
+
+Installing
+------------
+
+Party Mix works with Firefox and Google Chrome.  If you are using Firefox you
+will need to install the [Greasemonkey][] extension before proceeding.
+
+[Greasemonkey]: https://addons.mozilla.org/en-US/firefox/addon/748/  "Greasemonkey"
+
+[Click here to install the script.][install]
+
+
+Usage
+------
+
+Open or reload Rdio and navigate to your collection.  You should see a button
+above the list of artists in your collection that says "Add 25 random songs to
+Queue".
+
+
+License
+--------
+
+The MIT License
+
+Copyright (c) 2010 Jesse Hallett
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ party_mix_for_rdio.user.js
@@ -0,0 +1,183 @@
+// ==UserScript==
+// @name         Party Mix for Rdio
+// @namespace    http://sitr.us/
+// @description  Adds an "Add 25 random songs to Queue" button on the Rdio Collection view.
+// @include      http://www.rdio.com/*
+// ==/UserScript==
+
+var sitrus = function($) {
+    var exports = {}
+      , currentUser;
+
+    // Rdio views are loaded via ajax.  We can use the ajaxComplete event as a
+    // cue to check see if the Collection view has just been loaded.
+    $(document).bind('ajaxComplete', function() {
+        displayPartyMixButton();
+    });
+
+    function displayPartyMixButton() {
+        if ($('#party_mix').length < 1 && $('#collection_container').length > 0) {
+            var partyMixButton = $('<button id="party_mix">Add 25 random songs to Queue</button>');
+
+            partyMixButton.click(function() {
+                queueRandom(25);
+            });
+
+            $('#collection_container').prepend(partyMixButton);
+        }
+    }
+
+    // Adds n random songs from your collection to the queue.
+    function queueRandom(n) {
+        getArtistsInCollection(function(artists) {
+            for (var i = 0; i < n; i += 1) {
+                getRandomSongBy(randomMember(artists), addToQueue);
+            }
+        });
+    }
+
+    // Creates a playlist containing all of the songs in your collection.
+    function collectionShuffle() {
+        var name = 'collection shuffle '+ new Date();
+
+        getSongsInCollection(function(songs) {
+            createPlaylist(name, songs[0], function(isSuccess) {
+                getPlaylist(name, function(playlist) {
+                    songs.slice(1).forEach(function(song) {
+                        addToPlaylist(playlist, song, function() {});
+                    });
+                });
+            });
+        });
+    }
+
+    function randomMember(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+
+    function getRandomSongBy(artist, callback) {
+        getSongsForArtistInCollection(artist, function(songsByArtist) {
+            callback(randomMember(songsByArtist));
+        });
+    }
+
+    function getCollectionURL() {
+        return $('#header a:contains(Collection)').attr('href');
+    }
+
+    function getCollection(callback) {
+        $.getJSON(getCollectionURL(), callback);
+    }
+
+    function getCurrentUser(callback) {
+        if (currentUser) {
+            callback(currentUser);
+        } else {
+            getCollection(function(collection) {
+                currentUser = collection.content.content.data.puser;
+                callback(currentUser);
+            });
+        }
+    }
+
+    function getPlaylists(callback) {
+        getCollection(function(collection) {
+            $.getJSON(collection.content.content.data.urls.playlists, callback);
+        });
+    }
+
+    function getPlaylist(name, callback) {
+        getPlaylists(function(playlists) {
+            var lists = playlists.content.content.data.playlists;
+
+            callback(lists.filter(function(list) {
+                return list.name === name;
+            })[0]);
+        });
+    }
+
+    function createPlaylist(name, song, description, callback) {
+        if (typeof description == 'function') {
+            callback = description;
+            description = null;
+        }
+
+        $.post('/api/json/createPlaylist/', {
+            name: name,
+            song: song.id || song,
+            description: description || 'automatically generated playlist'
+        }, function(isSuccess) {
+            callback(isSuccess);
+        }, 'json');
+    }
+
+    function addToPlaylist(playlist, song, callback) {
+        $.getJSON('/api/json/addToPlaylist/', {
+            id: playlist.id || playlist,
+            song: song.id || song
+        }, function(isSuccess) {
+            callback(isSuccess);
+        });
+    }
+
+    function addToQueue(song) {
+        getPlayer()._queueSource('t', song.id || song);
+    }
+
+    function getArtistsInCollection(callback) {
+        getCurrentUser(function(user) {
+            $.getJSON('/api/json/getArtistsInCollection/', { uid: user.id }, callback);
+        });
+    }
+
+    function getSongsForArtistInCollection(artist, callback) {
+        getCurrentUser(function(user) {
+            $.getJSON('/api/json/getSongsForArtistInCollection/', {
+                id: artist.id || artist, uid: user.id
+            }, callback);
+        });
+    }
+
+    function getSongsInCollection(callback) {
+        getArtistsInCollection(function(artists) {
+            var syncCount = artists.length
+              , songs = [];
+
+            artists.forEach(function(artist) {
+                getSongsForArtistInCollection(artist, function(songsByArtist) {
+                    songs.push.apply(songs, songsByArtist);
+                    syncCount -= 1;
+
+                    if (syncCount === 0) {
+                        callback(songs);
+                    }
+                });
+            });
+        });
+    }
+
+    exports.displayPartyMixButton          = displayPartyMixButton;
+    exports.queueRandom                    = queueRandom;
+    exports.collectionShuffle              = collectionShuffle;
+    exports.getCollectionURL               = getCollectionURL;
+    exports.getCollection                  = getCollection;
+    exports.getCurrentUser                 = getCurrentUser;
+    exports.getPlaylists                   = getPlaylists;
+    exports.getPlaylist                    = getPlaylist;
+    exports.createPlaylist                 = createPlaylist;
+    exports.addToPlaylist                  = addToPlaylist;
+    exports.addToQueue                     = addToQueue;
+    exports.getArtistsInCollection         = getArtistsInCollection;
+    exports.getSongsForArtistInCollection  = getSongsForArtistInCollection;
+    exports.getSongsInCollection           = getSongsInCollection;
+
+    return exports;
+};
+
+(function() {
+    // Inject script so that in will run in the same context as other scripts
+    // on the page.
+    var s = document.createElement('script');
+    s.innerHTML = 'sitrus = ('+ sitrus.toString() +')(jQuery);';
+    document.body.appendChild(s);
+})();
